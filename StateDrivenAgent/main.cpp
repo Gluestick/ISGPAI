3e99088e33ff7ba2507c9dbbb1cada6eb91e9f91
Joep Kemperman <kazooiej@gmail.com>
2014-04-08 22:02:29 +0200

    The main function now creates a world object and updates it every second forever.
    NOTE: C++11 features are used for sleeping instead of win32.

--- StateDrivenAgent/main.cpp
+++ StateDrivenAgent/main.cpp
@@ -1,4 +1,16 @@
+#include "World.h"
+#include <thread>
+
+// Time between updates in ms.
+const int IntervalTime = 1000;
+
 int main(int argc, char **argv)
 {
+	SDA::World world;
+	while (true)
+	{
+		world.Update();
+		std::this_thread::sleep_for(std::chrono::milliseconds(IntervalTime));
+	}
 	return 0;
 }
