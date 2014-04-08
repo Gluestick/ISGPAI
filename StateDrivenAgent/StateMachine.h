6f70296e2ac226f8e76e9c0d9ed3a68aedaebb43
Joep Kemperman <kazooiej@gmail.com>
2014-04-08 12:56:05 +0200

    Added more method definitions to statemachine.

--- StateDrivenAgent/StateMachine.h
+++ StateDrivenAgent/StateMachine.h
@@ -1,9 +1,18 @@
 #pragma once
 
+#include "GameCharacter.h"
+#include "State.h"
+
 namespace SDA
 {
 	template <class T>
 	class StateMachine
 	{
+	public:
+		StateMachine(T* entity);
+		~StateMachine();
+		void ChangeState(&State<T> newState);
+	private:
+		T* _entity;
 	};
 }
