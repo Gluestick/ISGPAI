c95ec847608e5d72bee7c95030a502817cab9cad
Joep Kemperman <kazooiej@gmail.com>
2014-04-08 13:09:14 +0200

    Added testcode in main.cpp. Implemented con/destructor in statemachine.

--- StateDrivenAgent/StateMachine.h
+++ StateDrivenAgent/StateMachine.h
@@ -9,9 +9,17 @@ namespace SDA
 	class StateMachine
 	{
 	public:
-		StateMachine(T* entity);
-		~StateMachine();
-		void ChangeState(&State<T> newState);
+		StateMachine(T* entity)
+		{
+			this->_entity = entity;
+		}
+
+		~StateMachine()
+		{
+			// We do not own _entity. It must be deleted outside of this class.
+		}
+
+		void ChangeState(State<T>* newState);
 	private:
 		T* _entity;
 	};
--- StateDrivenAgent/main.cpp
+++ StateDrivenAgent/main.cpp
@@ -1,4 +1,9 @@
+#include "AttackState.h"
+
+using namespace SDA;
+
 int main(int argc, char **argv)
 {
+	AttackState state;
 	return 0;
 }
